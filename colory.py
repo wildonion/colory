08b02a19bb8bb36631ef276ea7508dcfd868ab6d
Erfan Arefi <marefimoghaddam@unixerr.com>
2021-01-01 16:36:04 +0330

    update argument options

--- colory.py
+++ colory.py
@@ -18,9 +18,10 @@
 |
 | USAGE : 
 |			python colory.py --adj-mat utils/adj_mat.txt --colors o r b \
-|							 --chromosomes 200 --generations 15 --parents 10 \
-|							 --selection-method roulette_wheel --crossover-method multi_point \
-|							 --mutation-method flipping --mutation-rate 0.20 --crossover-rate 0.80
+|							 --chromosomes 50 --generations 15 --parents 10 \
+|							 --selection-method roulette_wheel --crossover-method 2_point \
+|							 --mutation-method creep --replacement_method generational_elitism \
+|							 --mutation-rate 0.20 --crossover-rate 0.80
 |			
 |
 |
@@ -49,11 +50,11 @@ parser.add_argument('--chromosomes', action='store', type=int, help='The number 
 parser.add_argument('--generations', action='store', type=int, help='The number of generations.', required=True)
 parser.add_argument('--parents', action='store', type=int, help='The number of parents to mate for breeding offspring.', required=True)
 parser.add_argument('--selection-method', action='store', type=str, help='Selection method for crossover operation (roulette_wheel, tournament or rank).', required=True)
-parser.add_argument('--crossover-method', action='store', type=str, help='Crossover method to generate offspring (n_point, uniform, pmx or ox).', required=True)
+parser.add_argument('--crossover-method', action='store', type=str, help='Crossover method to generate offspring (n_point[where n is an integer], uniform, pmx or ox).', required=True)
 parser.add_argument('--mutation-method', action='store', type=str, help='Mutation method to mutate offspring (swap, creep, reversing or interchanging).', required=True)
 parser.add_argument('--replacement-method', action='store', type=str, help='Replacement method to replace the old population (generational_elitism, generational_gap, or steady_state).', required=True)
-parser.add_argument('--mutation-rate', action='store', type=float, help='Mutation rate.', required=True)
-parser.add_argument('--crossover-rate', action='store', type=float, help='Crossover rate.', required=True)
+parser.add_argument('--mutation-rate', action='store', type=float, help='Mutation rate (between 0.01 and 0.05 based on 20 <= chromosomes <= 30). You can use 1/chromosomes to setup the ratio.', required=True)
+parser.add_argument('--crossover-rate', action='store', type=float, help='Crossover rate (between 0.75 and 0.95 based on 20 <= chromosomes <= 30).', required=True)
 args = parser.parse_args()
 
 
