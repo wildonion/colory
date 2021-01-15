246613b63390ac00a6e5005034267cc4d6e61d72
Erfan Arefi <marefimoghaddam@unixerr.com>
2021-01-15 22:04:32 +0330

    - remove the part of checking if numpy arrays path are exist - update statistical logging section

--- colory.py
+++ colory.py
@@ -16,9 +16,9 @@
 | Graph Coloring With Minimum Number of Colors Using Genetic Algorithm
 |-------------------------------------------------------------------------------------------------
 |
-| USAGE : 
-|			python colory.py --adj-mat utils/adj_mat.txt --colors o r b \
-|							 --chromosomes 50 --generations 20 --parents 30 \
+| USAGE EXAMPLE : 
+|			python colory.py --adj-mat utils/matrices/tree/adj_mat.txt --colors o r b g \
+|							 --chromosomes 50 --generations 60 --parents 35 \
 |							 --selection-method tournament --crossover-method 3-point \
 |							 --mutation-method creep --alpha-rate 20 \
 |							 --mutation-rate 0.20 --crossover-rate 0.80
@@ -62,11 +62,8 @@ args = parser.parse_args()
 
 
 
-# ------------ defining the paths and building adjacency matrix
-# -------------------------------------------------------------------------
-VALID_CHROMOSOMES_PATH 				  = f"valid_chromo_in_{args.generations}_generations.npy"
-MINIMUM_COLORS_VALID_CHROMOSOMES_PATH = f"minimum_colors_valid_chromo_in_{args.generations}_generations.npy"
-BEST_SCORES_PATH                      = f"best_fitness_scores_in_{args.generations}_generations.npy"
+# ------------ building adjacency matrix
+# -----------------------------------------------
 ADJ_MAT                               = np.array([list(map(lambda x : int(x), list(filter(lambda x: x != '', \
 													[x for x in row.replace('\n', '').split(" ")])))) for row in args.adj_mat.readlines()])
 COLORS                                = np.array(args.colors)
@@ -104,19 +101,6 @@ elif COLORS.shape[0] < len(ADJ_MAT):
 	# -----------------------------------------
 	print(f"\n\t🧬 third gene of third chromosome with length {len(pop[2])} is ::: {pop[2].gene_objects[2].allele}")
 
-
-
-
-
-
-
-	# ------------ loading best chromosomes and best fitness scores
-	# --------------------------------------------------------------------
-	if os.path.exists(f"utils/+{VALID_CHROMOSOMES_PATH}") and os.path.exists(f"utils/+{BEST_SCORES_PATH}"): # load saved chromosomes and fitness scores
-		print(f"\n‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗loading best chromosomes and fitness scores in each generation‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗\n")
-		valid_chromosomes = np.load(VALID_CHROMOSOMES_PATH)
-		best_fitness_scores = np.load(BEST_SCORES_PATH)
-		minimum_colors_valid_chromosomes = np.load(MINIMUM_COLORS_VALID_CHROMOSOMES_PATH)
 	
 
 
@@ -125,63 +109,59 @@ elif COLORS.shape[0] < len(ADJ_MAT):
 
 	# ------------ running a genetic process to solve our problem
 	# --------------------------------------------------------------------
-	else: # create a genetic process
-		print(f"\n‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗finding minimum valid colors for each node through a genetic process‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗\n")
-		app = genetic_process(generations=args.generations, population=pop, parents=args.parents, selection_method=args.selection_method, 
-						  	  crossover_method=args.crossover_method, mutation_method=args.mutation_method, alpha_rate=args.alpha_rate,
-						  	  mutation_rate=args.mutation_rate, crossover_rate=args.crossover_rate)
-		app.run() # run the process
-		app.save() # save valid chromosomes, minimum valid chromosomes and best fitness scores
-		# app.plot() # plot fitness score in each generation after finishing the process 
-		app.draw() # draw the colored graph with best chromosome
-		valid_chromosomes = app.valid_chromosomes # all valid chromosomes in every generation
-		best_fitness_scores = app.best_fitness_scores # all best fitness scores in every generation
-		minimum_colors_valid_chromosomes = app.minimum_colors_valid_chromosomes # all minimum valid chromosomes
-
-
-
-
-		'''
-		≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣
-		 	 genetic process public attributes
-
-		app.adj_mat 						 = population.get_adj_mat()
-		app.colors 						     = population.get_colors()
-		app.generations 					 = generations
-		app.population 					     = population
-		app.parents 						 = parents
-		app.mutation_rate 					 = mutation_rate
-		app.crossover_rate 				  	 = crossover_rate
-		app.selection_method 				 = selection_method
-		app.crossover_method 				 = crossover_method
-		app.mutation_method 				 = mutation_method
-		app.alpha_rate 					  	 = alpha_rate
-		app.genes_population_after_fitness   = None
-		app.population_after_selection       = None
-		app.population_after_crossover       = None
-		app.population_after_mutation        = None
-		app.valid_chromosomes                = None
-		app.minimum_colors_valid_chromosomes = None
-		app.best_fitness_scores              = []
-
-		≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣
-		'''
+	print(f"\n‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗finding minimum valid colors for each node through a genetic process‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗\n")
+	app = genetic_process(generations=args.generations, population=pop, parents=args.parents, selection_method=args.selection_method, 
+						  crossover_method=args.crossover_method, mutation_method=args.mutation_method, alpha_rate=args.alpha_rate,
+						  mutation_rate=args.mutation_rate, crossover_rate=args.crossover_rate)
+	app.run() # run the process
+	app.save() # save valid chromosomes, minimum valid chromosomes and best fitness scores
+	# app.plot() # plot fitness score in each generation after finishing the process 
+	app.draw() # draw the colored graph with best chromosome
+	best_fitness_scores 			 				   = app.best_fitness_scores # all best fitness scores collected from every generation
+	total_generations_valid_chromosomes 			   = app.total_generations_valid_chromosomes # all valid chromosomes collected from every generation
+	total_generations_minimum_colors_valid_chromosomes = app.total_generations_minimum_colors_valid_chromosomes # all valid chromosomes with minimum colors collected from every generation
 
 
 
 
 
+	'''
+	≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣
+		 genetic process public attributes
 
-	# ------------ logging statistical infos of the genetic process  
-	# ------------------------------------------------------------------
-	for i in range(len(valid_chromosomes)):
-		print(f"\t🔬 Generation {i+1} Valid Chromosomes --- {valid_chromosomes[i]}")
-	print("\t➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤")
-	for i in range(len(minimum_colors_valid_chromosomes)):
-		print(f"\t🔬 Generation {i+1} Minimum Colors Valid Chromosomes --- {minimum_colors_valid_chromosomes[i]}")
-	print("\t➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤")
-	for i in range(len(best_fitness_scores)):
-		print(f"\t🔬 Generation {i+1} Best Fitness Score --- {best_fitness_scores[i]}")	
-	print("\t➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤")
-	print('\t▶ Average accepted score                   = ', np.mean(best_fitness_scores))
-	print('\t▶ Median score for accepted fitness scores = ', np.median(best_fitness_scores))
+	app.adj_mat
+	app.colors
+	app.graph    
+	app.generations
+	app.population    
+	app.parents
+	app.mutation_rate
+	app.crossover_rate
+	app.selection_method
+	app.crossover_method
+	app.mutation_method
+	app.alpha_rate
+	app.genes_population_after_fitness
+	app.population_after_selection
+	app.population_after_crossover
+	app.population_after_mutation
+	app.total_generations_valid_chromosomes
+	app.total_generations_minimum_colors_valid_chromosomes
+	app.best_fitness_scores
+
+	≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣
+	'''
+
+
+
+
+
+
+# ------------ logging statistical infos of the genetic process  
+# ------------------------------------------------------------------
+print('\t▶ Average Fitness Scores --- ', np.mean(best_fitness_scores))
+print('\t▶ Median Fitness Scores --- ', np.median(best_fitness_scores))
+if total_generations_valid_chromosomes.size > 0: # if there are some valid chromosomes 
+	print(f"\t▶ Total Valid Chromosomes in Generation 1 --- {total_generations_valid_chromosomes[0].shape}")
+if total_generations_minimum_colors_valid_chromosomes.size > 0: # if there are some valid chromosomes with minimum colors
+	print(f"\t▶ Total Valid Chromosomes with Minimum Colors in Generation 1 --- {total_generations_minimum_colors_valid_chromosomes[0].shape}\n")
